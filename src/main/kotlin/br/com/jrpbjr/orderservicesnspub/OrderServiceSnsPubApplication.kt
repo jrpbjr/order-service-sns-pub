b8eccedc0fced9d673da7b6509e7b0f2bf359c57
jrpbjr <jrpbjr@gmail.com>
2025-07-11 13:15:03 -0300

    inicio do projeto

---
+++ src/main/kotlin/br/com/jrpbjr/orderservicesnspub/OrderServiceSnsPubApplication.kt
@@ -0,0 +1,11 @@
+package br.com.jrpbjr.orderservicesnspub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderServiceSnsPubApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrderServiceSnsPubApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/jrpbjr/orderservicesnspub/OrderServiceSnsPubApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.jrpbjr.orderservicesnspub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrderServiceSnsPubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
